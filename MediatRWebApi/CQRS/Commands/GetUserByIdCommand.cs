a06b91d6a80ee1dadca8c5ba458f55dafb422dcf
berdikulov-571 <mygithub571@gmail.com>
2023-11-25 12:05:01 +0500

    GetUserByIdCommand

---
+++ MediatRWebApi/CQRS/Commands/GetUserByIdCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using MediatRWebApi.Entities;
+
+namespace MediatRWebApi.CQRS.Commands
+{
+    public class GetUserByIdCommand : IRequest<User>
+    {
+        public int UserId { get; set; }
+    }
+}
