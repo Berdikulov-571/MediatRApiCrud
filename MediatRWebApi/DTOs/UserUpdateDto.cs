e21264a0371bc3b548eb5fda18e77cd1f20998ce
berdikulov-571 <mygithub571@gmail.com>
2023-11-25 12:05:35 +0500

    UserUpdateDto

---
+++ MediatRWebApi/DTOs/UserUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MediatRWebApi.DTOs
+{
+    public class UserUpdateDto
+    {
+        public int UserId { get; set; }
+        public string firstName { get; set; } = default!;
+        public string lastName { get; set; } = default!;
+        public int Age { get; set; } = default!;
+    }
+}
