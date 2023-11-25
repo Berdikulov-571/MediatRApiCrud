844d76862ab24f3bd78869df25fc6d8323a04541
berdikulov-571 <mygithub571@gmail.com>
2023-11-25 12:03:39 +0500

    CreateUserCommandHandler

---
+++ MediatRWebApi/CQRS/CommandHandlers/CreateUserCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using MediatRWebApi.CQRS.Commands;
+using MediatRWebApi.DataAccess;
+using MediatRWebApi.Entities;
+
+namespace MediatRWebApi.CQRS.CommandHandlers
+{
+    public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand,int>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CreateUserCommandHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        async Task<int> IRequestHandler<CreateUserCommand, int>.Handle(CreateUserCommand request, CancellationToken cancellationToken)
+        {
+            User user = new User();
+            user.firstName = request.firstName;
+            user.lastName = request.lastName;
+            user.Age = request.Age;
+
+            await _context.Users.AddAsync(user);
+            int result = await _context.SaveChangesAsync(cancellationToken);
+
+            return result;
+        }
+    }
+}
