c4a6eb1bf641d9f97531c2b4a135819b735299e2
berdikulov-571 <mygithub571@gmail.com>
2023-11-25 12:04:13 +0500

    GetUserByIdCommandHandler

---
+++ MediatRWebApi/CQRS/CommandHandlers/GetUserByIdCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using MediatRWebApi.CQRS.Commands;
+using MediatRWebApi.DataAccess;
+using MediatRWebApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediatRWebApi.CQRS.CommandHandlers
+{
+    public class GetUserByIdCommandHandler : IRequestHandler<GetUserByIdCommand, User>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GetUserByIdCommandHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> Handle(GetUserByIdCommand request, CancellationToken cancellationToken)
+        {
+            User? user = await _context.Users.FirstOrDefaultAsync(x => x.UserId == request.UserId);
+
+            return user;
+        }
+    }
+}
