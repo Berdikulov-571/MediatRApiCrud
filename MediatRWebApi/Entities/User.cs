da1aa94ae9ea2fa662ce17ebb3fa052c30293406
berdikulov-571 <mygithub571@gmail.com>
2023-11-25 12:05:49 +0500

    UserModel

---
+++ MediatRWebApi/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MediatRWebApi.Entities
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string firstName { get; set; } = default!;
+        public string lastName { get; set; } = default!;
+        public int Age { get; set; } = default!;
+    }
+}
