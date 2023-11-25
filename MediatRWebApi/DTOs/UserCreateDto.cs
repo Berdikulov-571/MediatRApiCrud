ac7f9f75a94882af29432823622d2a11df29a574
berdikulov-571 <mygithub571@gmail.com>
2023-11-25 12:05:27 +0500

    UserCreateDto

---
+++ MediatRWebApi/DTOs/UserCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MediatRWebApi.DTOs
+{
+    public class UserCreateDto
+    {
+        public string firstName { get; set; } = default!;
+        public string lastName { get; set; } = default!;
+        public int Age { get; set; } = default!;
+    }
+}
