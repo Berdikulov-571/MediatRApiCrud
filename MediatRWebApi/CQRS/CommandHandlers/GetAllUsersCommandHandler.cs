b7eb834ddc04779e617ccda97930ce06c6fb6e5e
berdikulov-571 <mygithub571@gmail.com>
2023-11-25 12:04:24 +0500

    GetAllUsersCommandHandler

---
+++ MediatRWebApi/CQRS/CommandHandlers/GetAllUsersCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using MediatRWebApi.CQRS.Commands;
+using MediatRWebApi.DataAccess;
+using MediatRWebApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediatRWebApi.CQRS.CommandHandlers
+{
+    public class GetAllUsersCommandHandler : IRequestHandler<GetAllUsersCommand, IEnumerable<User>>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GetAllUsersCommandHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<User>> Handle(GetAllUsersCommand request, CancellationToken cancellationToken)
+        {
+            IEnumerable<User> users = await _context.Users.ToListAsync(cancellationToken);
+
+            return users;
+        }
+    }
+}
