66c2985bbe2b57d07ec70afd4d1c0435c6e49ee1
berdikulov-571 <mygithub571@gmail.com>
2023-11-25 12:05:57 +0500

    MediatR

--- MediatRWebApi/Program.cs
+++ MediatRWebApi/Program.cs
@@ -1,3 +1,8 @@
+using MediatR;
+using MediatRWebApi.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
 namespace MediatRWebApi
 {
     public class Program
@@ -6,16 +11,15 @@ namespace MediatRWebApi
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
@@ -25,7 +29,6 @@ namespace MediatRWebApi
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
